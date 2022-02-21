6abf6d061dc4db2726eaa7a91e46515ff4f92052
qiux.mac <qiux166036@gmail.com>
2022-02-21 15:22:41 +0800

    打印正三角型

---
+++ src/main/java/show/EquilateralTriangle.java
@@ -0,0 +1,24 @@
+package show;
+
+/**
+ * @author qiux
+ * @Date 2022/2/20
+ * @since
+ */
+public class EquilateralTriangle {
+
+    public static void main(String[] args) {
+        int max = 5;
+        for(int i=1; i<=max; i++) {
+          for (int j = 0; j <= max+i -1; j++) {
+              if (j <= (max - i)) {
+                  System.out.printf(" ");
+              } else {
+                  System.out.printf("*");
+              }
+          }
+            System.out.println();
+        }
+    }
+
+}
