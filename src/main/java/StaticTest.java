7489655891c8460a109586fc6116db303325a05f
qiux.mac <qiux166036@gmail.com>
2022-02-21 17:40:51 +0800

    static test

--- src/main/java/StaticTest.java
+++ src/main/java/StaticTest.java
@@ -3,7 +3,7 @@
  * @Date 2022/2/20
  * @since
  */
-public class StaticTest {
+public class StaticTest extends StaticSuperTest {
 
     static {
         System.out.println("static 1");
@@ -22,10 +22,24 @@ public class StaticTest {
     private static int count = 1;
 
     public static void main(String[] args) {
-//        StaticTest staticTest = new StaticTest();
+        StaticTest staticTest = new StaticTest();
         test();
         System.out.println("count:" + count);
 
     }
 
+}
+
+class StaticSuperTest{
+    static {
+        System.out.println("super static 1");
+    }
+
+    public static void test() {
+        System.out.println("super static 2");
+    }
+
+    {
+        System.out.println("super 3");
+    }
 }
